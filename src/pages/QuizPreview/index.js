ea2786ccb255f6b77bf61d7ce9810c9d4c0465c0
Dimitar Mirchev <dirchev@camplight.net>
2019-02-26 22:30:12 +0000

    Rework QuizPreview page

--- src/pages/QuizPreview/index.js
+++ src/pages/QuizPreview/index.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react"
 import { connect } from "react-redux";
-import { Link } from 'react-router-dom'
 import { prepareQuiz } from 'actions/quiz'
-import MultilineText from "../../components/MultilineText";
+import QuizEngagements from './QuizEngagements'
+import QuizActions from "./QuizActions";
+import QuizDetails from "./QuizDetails";
+import './index.scss'
+
 
 class QuizPreview extends Component {
   componentWillMount () {
@@ -11,50 +14,16 @@ class QuizPreview extends Component {
 
   render() {
     return (
-      <div className="container">
-        <section>
-          <h1>{this.props.quiz.name}</h1>
-          <MultilineText text={this.props.quiz.description} />
-          <p>
-            You have <strong>{this.props.quiz.noOfAttempts}</strong> attempts for this quiz.
-            {
-              this.props.quiz.isMandatory
-              ? (
-                <span>The quiz is <strong>mandatory</strong>.</span>
-              ) : null
-            }
-          </p>
-          {
-            this.props.quiz.timeLimit
-            ? (
-              <p>This quiz is time limited. You have <strong>{this.props.quiz.timeLimit}</strong> minutes to complete. The time will start counting from the moment you see the first question.</p>
-            ) : null
-          }
-        </section>
-        <div className="controls separated">
-          <Link to="/dashboard" className="button button-grey">Back</Link>
-          {
-            this.renderEngageButton()
-          }
+      <div className="quiz-preview">
+        <div className="quiz-details">
+          <div className="box">
+            <QuizDetails quiz={this.props.quiz} />
+            <QuizActions quiz={this.props.quiz} />
+          </div>
         </div>
+        <QuizEngagements quiz={this.props.quiz} />
       </div>
     )
-  }
-
-  renderEngageButton () {
-    if (this.props.loading) {
-      return (
-        <span>Loading...</span>
-      )
-    } else if (this.props.error) {
-      return (
-        <div className="alert alert-danger">Error: {this.props.error.toString()}</div>
-      )
-    } else if (this.props.success) {
-      return (
-        <Link to={`/quiz/${this.props.quiz._id}/engage`} className="button button-primary">Engage Quiz</Link>
-      )
-    }
   }
 }
 
@@ -63,8 +32,6 @@ let mapStateToProps = (state, props) => {
   return {
     loading: state.loading.QUIZ_PREPARE,
     error: state.error.QUIZ_PREPARE,
-    success: state.success.QUIZ_PREPARE,
-    quizId: quizId,
     quiz: state.entities.quizess[quizId]
   }
 }
