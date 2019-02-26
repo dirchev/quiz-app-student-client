d67d5c170848cd1197965d969a1902c5982157f8
Dimitar Mirchev <dirchev@camplight.net>
2019-02-26 22:30:42 +0000

    add quizEngagement clear actions handling

--- src/reducers/quizEngagement.js
+++ src/reducers/quizEngagement.js
@@ -4,6 +4,9 @@ const quizEngagementReducer = (state = DEFAULT_STATE, action) => {
   switch (action.type) {
     case 'QUIZ_ENGAGEMENT_CREATE_SUCCESS':
       return action.payload.quizEngagement._id
+    case 'QUIZ_ENGAGEMENT_CANCEL_SUCCESS':
+    case 'QUIZ_ENGAGEMENT_END':
+      return null
     default:
       return state
   }
