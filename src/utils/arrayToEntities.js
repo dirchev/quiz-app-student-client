b369309b8d01c08a768e1547f06e1562c9b5e238
Dimitar Mirchev <dirchev@camplight.net>
2019-03-27 20:04:40 +0000

    Update how arrayToEntities processes __meta

--- src/utils/arrayToEntities.js
+++ src/utils/arrayToEntities.js
@@ -1,8 +1,17 @@
 export default function (items, state) {
   let entities = {}
-  items.forEach(item => entities[item._id] = {
-    ...(state ? state[item._id] : {}),
-    ...item,
+  items.forEach(item => {
+    let oldItemData = state ? (state[item._id] || {}) : {}
+    let oldItemMeta = oldItemData.__meta || {}
+    entities[item._id] = {
+      ...oldItemData,
+      ...item,
+      // merge the meta information
+      __meta: {
+        ...oldItemMeta,
+        ...item.__meta
+      },
+    }
   })
   return entities
 }
